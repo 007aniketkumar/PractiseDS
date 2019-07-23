d4d297301a2be18835ee6ce3b3ae48c2dd2bb35e
aniket <007aniketkumar@gmail.com>
2019-07-23 08:53:18 +0530

    fixed inorder traversal

--- src/com/ani/trees/InorderTraversalUsingStack.java
+++ src/com/ani/trees/InorderTraversalUsingStack.java
@@ -50,35 +50,79 @@ public class InorderTraversalUsingStack {
 
         //put the root in stack first:
 
-        inorderStack.push(binaryTree);
+      // inorderStack.push(binaryTree);
         Node currentNode = binaryTree;
         //now keep iterating towards the left side
 
-        while (!inorderStack.empty()) {
+        while (!inorderStack.empty()|| currentNode!=null) {
 
             while (currentNode != null) {
-                currentNode = currentNode.leftChild;
                 inorderStack.push(currentNode);
+                currentNode = currentNode.leftChild;
             }
 
             //once you have reached the end of while loop, pop the last item from the stack and print the left most child
 
             currentNode = inorderStack.pop();
-            System.out.println(currentNode.data);
+            System.out.println("" + currentNode.data);
 
-            //also iterate on the right side of the popped item , the popped left child can have right children of its own.
-
-            while (currentNode.rightChild != null) {
-                currentNode = currentNode.rightChild;
-                inorderStack.push(currentNode);
+            //also check on the right side of the popped item , the popped left child can have right children of its own.
+            currentNode = currentNode.rightChild;
             }
         }
 
 
+
+
+
+
+
+     //Print the tree
+    static void print(Node root) {
+
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data);
+        print(root.leftChild);
+        print(root.rightChild);
     }
 
 
-}
+
+
+    static void printUsingStack(Node root) {
+
+        Stack<Node> printingStack = new Stack<>();
+        Node currentNode = root;
+
+        if(root==null){
+            return;
+        }
+
+
+        while(!printingStack.empty()|| currentNode!=null) {
+
+            while(currentNode!=null) {
+
+                printingStack.push(currentNode);
+                currentNode = currentNode.leftChild;
+            }
+
+            //if there are no items on left side , start popping them and print the nodes on the right hand side now
+
+            currentNode = printingStack.pop();
+            System.out.println(currentNode);//print the top most item in the stack
+
+            //point towards the right node and then check for its children
+            currentNode = currentNode.rightChild;
+            }
+
+
+        }
+    }
+
+
 
 
 
